006d2f2ec8a789edcd2b717e5f14ae855cf9177d
Sebastien Tschappat <djtalfx@gmail.com>
2019-08-06 18:11:28 +0200

    exo 2 done without style

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    # @task.update(task_params)
+
+    # no need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
